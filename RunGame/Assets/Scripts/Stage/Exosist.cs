6feafb167dc9185a4040afd5eeeb45e3dca78c56
Yodai-Toda <2001daiyousama@gmail.com>
2020-09-24 00:10:25 +0900

    ハゲのアニメーションの追加

--- RunGame/Assets/Scripts/Stage/Exosist.cs
+++ RunGame/Assets/Scripts/Stage/Exosist.cs
@@ -6,13 +6,19 @@ namespace RunGame.Stage
 {
     public class Exosist : MonoBehaviour
     {
+        float time = 0.0f;
+
         [SerializeField]
         private float speed = 4;
         Transform target;
-        
+
+        Animator Animator;
+       
+
         new Rigidbody2D rigidbody;
         void Start()
         {
+            Animator = GetComponent<Animator>();
             target = GameObject.FindGameObjectWithTag("Player").transform;
             // 事前にコンポーネントを参照
             rigidbody = GetComponent<Rigidbody2D>();
@@ -22,14 +28,26 @@ namespace RunGame.Stage
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
+            time += Time.deltaTime;
             var velocity = rigidbody.velocity;
             velocity.x = speed;
             rigidbody.velocity = velocity;
             if (target == null)
             {
                 speed = 0;
+                Animator.SetBool("Jorei", true);
+            }
+            if(time >=  5.0f)
+            {
+                Animator.SetFloat("Time", time);
+                
+            }
+            if (time >= 5.1f)
+            {
+                time = 0.0f;
+                Animator.SetFloat("Time", time);
             }
         }
     }
